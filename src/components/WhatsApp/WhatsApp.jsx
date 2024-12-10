76b8fa8c4250b22898eb77d7d43ef2bca82c1b0b
samalsagar <sagar.samal6264@gmail.com>
2024-12-10 18:37:00 +0530

    Number updated

--- src/components/WhatsApp/WhatsApp.jsx
+++ src/components/WhatsApp/WhatsApp.jsx
@@ -4,7 +4,7 @@ function WhatsApp() {
   return (
     <div>
 
-    <a href="https://wa.me/919810105099?text=How can I help you ?" target='_blank' id="whatsapp-button">
+    <a href="https://wa.me/919348228808?text=How can I help you ?" target='_blank' id="whatsapp-button">
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/640px-WhatsApp_icon.png" alt="WhatsApp" />
     </a>
 
